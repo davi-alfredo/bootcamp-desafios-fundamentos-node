fb11343d8b7a6f2c2efe68548efac0e70bf1f510
DavidAlfredo <davi.alfredo@hotmail.com>
2021-01-02 18:06:21 -0300

    Desafio Fundamentos Node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,51 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    /** FORMA ENSINADA COM REDUCE */
+    // const { income, outcome } = this.transactions.reduce(
+    //   (accumulador: Balance, transaction: Transaction) => {
+    //     switch (transaction.type) {
+    //       case 'income':
+    //         accumulador.income += transaction.value;
+    //         break;
+    //       case 'outcome':
+    //         accumulador.outcome += transaction.value;
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     return accumulador;
+    //   },
+    //   {
+    //     income: 0,
+    //     outcome: 0,
+    //     total: 0,
+    //   },
+    // );
+
+    let income = 0;
+    let outcome = 0;
+    this.transactions.filter(transaction => {
+      if (transaction.type === 'income') {
+        income += transaction.value;
+      } else {
+        outcome += transaction.value;
+      }
+      return transaction;
+    });
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
